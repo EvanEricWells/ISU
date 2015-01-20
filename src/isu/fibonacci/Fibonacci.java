409f903b904eb6b1831121bb13178825859cad0a
evan5090 <evan5090@ISS147D8-19.scdsb.pri>
2015-01-20 08:25:40 -0500

    Finds fibonacci numbers recursively

---
+++ src/isu/fibonacci/Fibonacci.java
@@ -0,0 +1,28 @@
+package isu.fibonacci;
+
+
+import TerminalIO.*;
+
+public class Fibonacci {
+    public static void main(String args[]){
+        KeyboardReader read = new KeyboardReader();
+        int result;
+        int value;
+        System.out.print("Please Enter a number between 1 and 15: ");
+        value = read.readInt();
+        while (true){
+            if(value>=1 && value<=15){
+                result = fibonacci(value);
+                break;
+            }
+            else System.out.println("Incorrect Number must be between 1 and 15");
+            value=read.readInt();
+        }
+        System.out.println("The number is: "+ result);
+        
+    }
+     public static int fibonacci(int n){
+        if(n<=2) return 1;
+        else return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
